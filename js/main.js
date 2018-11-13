e50d33bf790482d69c288ba025f24e7398448fe6
Christina <kristina3531@gmail.com>
2018-11-13 11:56:03 +0200

    update masonry

--- js/main.js
+++ js/main.js
@@ -1,107 +1,101 @@
 $(document).ready(function() {
+    $("a, button").click(function(e) {
+        e.preventDefault();
+    });
 
-	//Add class active in header-menu and tabs
-	$(".header-menu__link, .tabBtn-1, .tabBtn-2").on("click", function() {
-		$(".header-menu__link.active, .tabBtn-1.active, .tabBtn-2.active").removeClass("active");
-		$(this).toggleClass("active");
-	});
+    //Show search-bar
+    $(".header-form__search").on("click", function() {
+        $(".header-form").toggleClass("open");
+    });
 
-	//Show search-bar
-	$(".header-form__search").on("click", function(){
-		$(".header-form").toggleClass("open");
-	});
+    //Add class active in header-menu__link
+    $(".header-menu__link").on("click", function() {
+        $(".header-menu__link.active").removeClass("active");
+        $(this).toggleClass("active");
+    });
 
-//    $('tabBtn:not(:first)').hide();
-//    $('tabContent:not(:first)').hide();
-	//Tabs
-	$(".tabBtn-1").on("click", function() {
-		$(".tabContent-1").hide();
-		$(".tabBtn-1.selected").removeClass("selected");
-		$(this).toggleClass("selected");
-		let tabId = $(this).attr("data-tab");
-		$("#" + tabId).show();
-	});
-    
+    //Tabs
+    $(".tabBtn-1").on("click", function() {
+        $(".tabContent-1").hide();
+        $(".tabBtn-1.selected").removeClass("selected");
+        $(this).toggleClass("selected");
+        let tabId = $(this).attr("data-tab");
+        $("#" + tabId).show();
+    });
+
     $(".tabBtn-2").on("click", function() {
-		$(".tabContent-2").hide();
-		$(".tabBtn-2.selected").removeClass("selected");
-		$(this).toggleClass("selected");
-		let tabId = $(this).attr("data-tab");
-		$("#" + tabId).show();
-	});
+        $(".tabContent-2").hide();
+        $(".tabBtn-2.selected").removeClass("selected");
+        $(this).toggleClass("selected");
+        let tabId = $(this).attr("data-tab");
+        $("#" + tabId).show();
+    });
 
-  // $(".header-menu__link:eq(0)").click();
-	$(".tabBtn-1:eq(0)").click();
-  $(".tabBtn-2:eq(0)").click();
+    $(".tabBtn-1:eq(0)").click();
+    $(".tabBtn-2:eq(0)").click();
 
+    let itemSize = 12;
+    let arrayElements = [];
+    let maxGalleryLength = arrayElements.length;
+    let count = 0;
 
-	let itemSize = 12;
-	let arrayElements = [];
-	let maxGalleryLength = arrayElements.length;
-	let count = 0;
+    function addElements() {
 
-	function addElements() {
-		
-        														
-		for(let i = 0; i < itemSize; i++) {
+        for (let i = 0; i < itemSize; i++) {
 
-	       //Create DOMs
-            
-            let workGalleryItem = $(`<div class="work-gallery__item"><div class="work-gallery__img"><img src = "img/graphic-design/graphic-design${count + 1}.jpg" alt="Image${count + 1}"></div><div class="work-gallery__info"><div class="work-gallery__icons"><a href="#" class="work-gallery__icon"><svg class="link"><use xlink:href="#link"></svg></a><a href="#" class="work-gallery__icon"><svg class="search"><use xlink:href="#search"></svg></a></div><h5 class="work-gallery__heading">creative design</h5><span class="work-gallery__cath">Graphic Design</span></div></div>`);
+            //Create DOMs
 
-			arrayElements.push(workGalleryItem);
-			count++;
-			if(count === maxGalleryLength) {
-				$("#loadMore").hide();
-			}
+            let galleryGraphicDesign = $(`<div class="work-gallery__item"><div class="work-gallery__img"><img src = "img/graphic-design/graphic-design${count + 1}.jpg" alt="Image${count + 1}"></div><div class="work-gallery__info"><div class="work-gallery__icons"><a href="#" class="work-gallery__icon"><svg class="link"><use xlink:href="#link"></svg></a><a href="#" class="work-gallery__icon"><svg class="search"><use xlink:href="#search"></svg></a></div><h5 class="work-gallery__heading">creative design</h5><span class="work-gallery__cath">Graphic Design</span></div></div>`);
 
-		} 
+            let galleryWebDesign = $(`<div class="work-gallery__item"><div class="work-gallery__img"><img src = "img/web-design/web-design${count + 1}.jpg" alt="Image${count + 1}"></div><div class="work-gallery__info"><div class="work-gallery__icons"><a href="#" class="work-gallery__icon"><svg class="link"><use xlink:href="#link"></svg></a><a href="#" class="work-gallery__icon"><svg class="search"><use xlink:href="#search"></svg></a></div><h5 class="work-gallery__heading">creative design</h5><span class="work-gallery__cath">Web Design</span></div></div>`);
 
-		$('#tab-7').append(arrayElements);
-	}
+            let galleryLandingPagedesign = $(`<div class="work-gallery__item"><div class="work-gallery__img"><img src = "img/landing-page/landing-page${count + 1}.jpg" alt="Image${count + 1}"></div><div class="work-gallery__info"><div class="work-gallery__icons"><a href="#" class="work-gallery__icon"><svg class="link"><use xlink:href="#link"></svg></a><a href="#" class="work-gallery__icon"><svg class="search"><use xlink:href="#search"></svg></a></div><h5 class="work-gallery__heading">creative design</h5><span class="work-gallery__cath">Landing Page</span></div></div>`);
 
-	addElements();
-	$("#loadMore").on("click", addElements);
-	
-//	//Slick-slider
-//	let aboutSlider = $(".about-slider");
-//	aboutSlider.slick({
-//		slidesToShow: 1,
-//		slidesToScroll: 1,
-//		arrows: false,
-//		fade: true,
-//		asNavFor: ".about-slider__nav-list"
-//	});
-//	$(".about-slider__nav-list").slick({
-//		centerMode: true,
-//		centerPadding: "80px",
-//		slidesToShow: 3,
-//		slidesToScroll: 1,
-//		asNavFor: ".about-slider",
-//		swipeToSlide: true,
-//		focusOnSelect: true,
-//		variableWidth: true,
-//		prevArrow: $(".about-slider__btn-prev"),
-//		nextArrow: $(".about-slider__btn-next")
-//	});
-//
-//	 $('a[data-slide]').click(function(e) {
-//   e.preventDefault();
-//   var slideno = $(this).data('slide');
-//   $('.about-slider__nav-list').slick('slickGoTo', slideno - 1);
-// });
-	
-	//Slider with tabs
-	$(".about-slider__nav-item").on("click", function() {
-		$(".about-slider__item").hide();
-		$(".about-slider__nav-item.selected").removeClass("selected");
-		$(this).toggleClass("selected");
-		let tabId = $(this).attr("data-tab");
-		$("#" + tabId).show();
-	});
-	
-	$(".about-slider__nav-item:eq(0)").click();
+            let galleryWordpressdesign = $(`<div class="work-gallery__item"><div class="work-gallery__img"><img src = "img/wordpress/wordpress${count + 1}.jpg" alt="Image${count + 1}"></div><div class="work-gallery__info"><div class="work-gallery__icons"><a href="#" class="work-gallery__icon"><svg class="link"><use xlink:href="#link"></svg></a><a href="#" class="work-gallery__icon"><svg class="search"><use xlink:href="#search"></svg></a></div><h5 class="work-gallery__heading">creative design</h5><span class="work-gallery__cath">Wordpress</span></div></div>`);
+
+            arrayElements.push(galleryGraphicDesign, galleryWebDesign, galleryLandingPagedesign, galleryWordpressdesign);
+            count++;
+            if (count === maxGalleryLength) {
+                $("#loadMore").hide();
+            }
+
+        }
+
+        $('#tab-7').append(arrayElements);
+    }
+
+    addElements();
+    $("#loadMore").on("click", addElements);
+
+    //Slider
+    $(".about-slider__item").not(":first").hide();
+    $(".about-slider__nav-img").click(function() {
+        let currentIndex = $(".about-slider__nav-img.active").index();
+        $(".about-slider__item").eq(currentIndex).hide();
+        $(".about-slider__nav-img.active").removeClass("active");
+        $(this).addClass("active");
+        $(".about-slider__item").eq($(this).index()).show();
+    });
+
+    //Slider buttons
+    $('.about-slider__btn-prev').click(function() {
+        let currentIndex = $('.about-slider__nav-img.active').index();
+        $(".about-slider__item").eq(currentIndex).hide();
+        $('.about-slider__nav-img.active').removeClass('active');
+        $('.about-slider__nav-img').eq(currentIndex - 1).addClass('active');
+        $(".about-slider__item").eq(currentIndex - 1).show();
+    });
+
+    $('.about-slider__btn-next').click(function() {
+        let currentIndex = $('.about-slider__nav-img.active').index();
+
+        $(".about-slider__item").eq(currentIndex).hide();
+        currentIndex = currentIndex === $('.about-slider__nav-img').length - 1 ? -1 : $('.about-slider__nav-img.active').index();
+        $('.about-slider__nav-img.active').removeClass('active');
+        $('.about-slider__nav-img').eq(currentIndex + 1).addClass('active');
+        $(".about-slider__item").eq(currentIndex + 1).show();
+    });
+
+    $('.gallery-wrap').masonry({});
 
 });
-
-
